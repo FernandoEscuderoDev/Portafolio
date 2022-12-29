f7349e1d2902989325a530b2437c948b9760532b
FernEscDev <lx13788@innovaccion.mx>
2022-12-29 12:27:31 -0300

    detalles

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -4,7 +4,7 @@ export default function NavMobile({event}) {
     <ul
       className={`sm:hidden transition-all ease-in duration-300 z-10 flex flex-col items-center justify-center gap-14 absolute
           ${event}
-          top-0  w-full h-full bg-zinc-800/80 text-xl font-bold`}
+          top-0  w-full h-screen bg-zinc-800/80 text-xl font-bold `}
     >
       <ItemsNav/>
       <li className="flex items-center justify-around w-36 text-4xl">
