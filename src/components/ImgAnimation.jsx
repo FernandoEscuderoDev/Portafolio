38fade7b7baab816452b927c3d0a8833fae90884
FernEscDev <lx13788@innovaccion.mx>
2022-12-31 20:19:14 -0300

    5

--- src/components/ImgAnimation.jsx
+++ src/components/ImgAnimation.jsx
@@ -3,9 +3,9 @@ export default function ImgAnimation({ children }) {
   return (
     <motion.div
       className="w-full"
-      initial={{scale:0, opacity: 0}}
+      initial={{scale:0.5, opacity: 0}}
       animate={{scale: 1}}
-      transition={{ duration: 1.3 }}
+      transition={{ duration: 1.5 }}
       whileInView={{opacity: 1 }}
     >
       {children}
