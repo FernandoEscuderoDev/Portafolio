eff581c08a2648579e82bea4cc1c0ae15bb4b380
FernEscDev <lx13788@innovaccion.mx>
2022-12-31 20:18:54 -0300

    5

--- src/components/ImgAnimation.jsx
+++ src/components/ImgAnimation.jsx
@@ -1,3 +1,14 @@
-export default function ImgAnimation(){
-  
+import { motion } from "framer-motion";
+export default function ImgAnimation({ children }) {
+  return (
+    <motion.div
+      className="w-full"
+      initial={{scale:0, opacity: 0}}
+      animate={{scale: 1}}
+      transition={{ duration: 1.3 }}
+      whileInView={{opacity: 1 }}
+    >
+      {children}
+    </motion.div>
+  );
 }
