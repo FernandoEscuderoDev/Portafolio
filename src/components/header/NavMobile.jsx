39296a3098c7f113bfee38a6a1d63bbb851628a3
FernEscDev <lx13788@innovaccion.mx>
2023-01-03 21:02:20 -0300

    1

--- src/components/header/NavMobile.jsx
+++ src/components/header/NavMobile.jsx
@@ -1,12 +1,29 @@
 import ItemsNav from "./ItemsNav.jsx";
-export default function NavMobile({event}) {
+import { motion } from "framer-motion";
+export default function NavMobile({ event }) {
+  const variants = {
+    open: {
+      x:"100vh"
+    },
+    closed: {
+      x: "0",
+      }
+    }
   return (
-    <ul
-      className={`sm:hidden transition-all ease-in duration-300 z-10 flex flex-col items-center justify-center gap-14 absolute
-          ${event}
-          top-0  w-full h-screen bg-zinc-800/80 text-xl font-bold `}
+    // <ul
+    //   className={`sm:hidden transition-all ease-in duration-300 z-10 flex flex-col items-center justify-center gap-14 absolute
+    //       ${event}
+    //       top-0  w-full h-screen bg-zinc-800/80 text-xl font-bold `}
+    //
+    //   <ItemsNav />
+    <motion.ul
+      className={`sm:hidden z-10 inline-flex flex-col items-center justify-center gap-14 absolute top-0  w-full h-screen bg-zinc-800/80 text-xl font-bold`}
+      initial={{}}
+      animate={{event}}
+      transition={{duration:1.2, type:"spring"}}
+      variants={{variants}}
     >
-      <ItemsNav/>
+      <ItemsNav />
       <li className="flex items-center justify-around w-36 text-4xl">
         <a>
           <i className="fa-brands fa-github"></i>
@@ -15,6 +32,7 @@ export default function NavMobile({event}) {
           <i className="fa-brands fa-linkedin"></i>
         </a>
       </li>
-    </ul>
+    </motion.ul>
+    // </ul>
   );
 }
