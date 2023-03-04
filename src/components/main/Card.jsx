01da10207f35095313def2222d616e79b42513a2
FernEscDev <hoopa8645@gmail.com>
2023-03-03 23:37:50 -0300

    a

--- src/components/main/Card.jsx
+++ src/components/main/Card.jsx
@@ -24,24 +24,36 @@ export default function Card() {
       content:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo minus hic nobis quidem nesciunt culpa exercitationem sed dolorem magni deleniti!",
     },
+    {
+      src: "https://picsum.photos/200/300?random=5",
+      title: "Astro",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo minus hic nobis quidem nesciunt culpa exercitationem sed dolorem magni deleniti!",
+    },
+    {
+      src: "https://picsum.photos/200/300?random=6",
+      title: "Astro",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo minus hic nobis quidem nesciunt culpa exercitationem sed dolorem magni deleniti!",
+    },
   ];
 
   return (
-    <>
+    <main className='grid grid-cols-3 gap-2'>
       {datos.map((dato) => (
-        <div key={dato} className="relative efectoTarjetaPrincipal shadow-xl shadow-zinc-900 rounded-xl w-64 h-80">
-          <div className="absolute h-full w-full flex items-center justify-evenly flex-col z-50 text-center text-zinc-100">
-            <img
-              src={dato.src}
-              className="object-cover rounded-full w-28 h-28 shadow-lg shadow-zinc-900"
-            />
-            <h1 className="font-bold text-2xl">{dato.title}</h1>
-            <p className="text-sm w-[80%]">{dato.content}
-            </p>
+        <div
+          key={dato}
+          className="relative efectoTarjetaPrincipal shadow-xl shadow-zinc-900 rounded-xl w-64 h-80">
+          <img
+            className="h-full w-full rounded-md object-cover"
+            src={dato.src}
+          />
+          <div className="absolute top-0 flex h-full w-full flex-col items-center justify-center gap-6 opacity-0 transition-all duration-500 hover:opacity-100 hover:backdrop-blur-sm">
+            <h3>Pomodoro</h3>
+            <p>Parrafo</p>
           </div>
-          <div className="efectoTarjeta rounded-xl w-full h-full"></div>
         </div>
       ))}
-    </>
+    </main>
   );
 }
